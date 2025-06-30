faa813f33dc61b9a33f9755d8c302cc181104252
DiscountHub <k.proj.2019@gmail.com>
2025-07-01 00:16:46 +0400

    Update main.py
    Подключён роутер discounts к FastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
+from app.routers import discounts
+
 from fastapi import FastAPI
 from app.api import router as api_router
 
 app = FastAPI(title="DiscountHub API")
 
 app.include_router(api_router)
+app.include_router(discounts.router)
+
 from app.init_db import init_db
 
 init_db()
