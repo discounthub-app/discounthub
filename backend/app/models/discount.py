688e83bcdc7e7ae3917e272ee8507f8806f46ae2
DiscountHub <k.proj.2019@gmail.com>
2025-07-01 00:03:50 +0400

    Update discount.py

--- backend/app/models/discount.py
+++ backend/app/models/discount.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from app.database import Base
 
 class Discount(Base):
@@ -7,3 +7,8 @@ class Discount(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String)
+    price = Column(Float)
+    original_price = Column(Float)
+    seller_id = Column(Integer, ForeignKey("sellers.id"))  # Продавцы могут быть отдельной таблицей
+    url = Column(String)
+    image_url = Column(String)
