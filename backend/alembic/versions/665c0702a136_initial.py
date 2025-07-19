f5607bfe19bc4333e5280b5910fed1cac86b29e2
root <root@v391863.hosted-by-vdsina.com>
2025-07-19 18:36:18 +0300

    feat: initial Alembic migration

---
+++ backend/alembic/versions/665c0702a136_initial.py
@@ -0,0 +1,22 @@
+"""initial
+
+Revision ID: 665c0702a136
+Revises: 
+Create Date: 2025-07-19 15:25:27.248623
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '665c0702a136'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
