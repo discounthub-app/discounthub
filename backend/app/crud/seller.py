d48c53c1ffc5007f1f708580a73199276fc28ca2
DiscountHub <k.proj.2019@gmail.com>
2025-07-25 01:02:27 +0400

    Update seller.py

--- backend/app/crud/seller.py
+++ backend/app/crud/seller.py
@@ -1,19 +1,15 @@
 from sqlalchemy.orm import Session
-from app.models.seller import Seller
-from app.schemas.seller import SellerCreate
+from app import models, schemas
 
-
-def create_seller(db: Session, seller: SellerCreate) -> Seller:
-    db_seller = Seller(name=seller.name)
+def create_seller(db: Session, seller: schemas.SellerCreate):
+    db_seller = models.Seller(name=seller.name)
     db.add(db_seller)
     db.commit()
     db.refresh(db_seller)
     return db_seller
 
-
-def get_sellers(db: Session):
-    return db.query(Seller).all()
-
-
 def get_seller(db: Session, seller_id: int):
-    return db.query(Seller).filter(Seller.id == seller_id).first()
+    return db.query(models.Seller).filter(models.Seller.id == seller_id).first()
+
+def get_sellers(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Seller).offset(skip).limit(limit).all()
