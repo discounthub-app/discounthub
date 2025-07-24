0fa10e741a104069303664fb81ee2c4a174cf115
DiscountHub <k.proj.2019@gmail.com>
2025-07-25 02:04:02 +0400

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,24 +1,24 @@
 from fastapi import FastAPI
 from sqlalchemy.orm import Session
 from sqlalchemy import text
-
 from app.db import SessionLocal
 from app.routers.discount import router as discount_router
 from app.routers.seller import router as seller_router
+from app.routers.user import router as user_router  # ✅ добавлено
 
 app = FastAPI()
 
-# Подключаем роутеры
+# Подключаем router-ы
 app.include_router(discount_router)
 app.include_router(seller_router)
+app.include_router(user_router)  # ✅ добавлено
 
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
 
-@app.get("/users")
+@app.get("/users/raw")
 def get_users():
     db: Session = SessionLocal()
     users = db.execute(text("SELECT id, username, email FROM users")).mappings().all()
-    db.close()
     return users
