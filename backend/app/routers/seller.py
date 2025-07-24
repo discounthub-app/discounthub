5110c90e7f6311f1dc57b47a6d6d762fc3a38dd2
DiscountHub <k.proj.2019@gmail.com>
2025-07-25 01:14:05 +0400

    Update seller.py

--- backend/app/routers/seller.py
+++ backend/app/routers/seller.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app import schemas, crud
 from app.db import get_db
+from app.crud import seller as crud
 
 router = APIRouter(
     prefix="/sellers",
