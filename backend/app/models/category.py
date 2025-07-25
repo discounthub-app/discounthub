d9eaa5d98f335d02451361ea355b7ee9d6d4635e
DiscountHub <k.proj.2019@gmail.com>
2025-07-25 13:26:22 +0400

    Update category.py

--- backend/app/models/category.py
+++ backend/app/models/category.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from app.db import Base
 
 class Category(Base):
@@ -6,3 +7,4 @@ class Category(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, nullable=False)
+    products = relationship("Product", back_populates="category")
