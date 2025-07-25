dff22943c0fd7e22cf84d7e0916f3dc630030591
DiscountHub <k.proj.2019@gmail.com>
2025-07-25 21:27:05 +0400

    Update city_region.py

--- backend/app/models/city_region.py
+++ backend/app/models/city_region.py
@@ -1,4 +1,7 @@
+# backend/app/models/city_region.py
+
 from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 from app.db import Base
 
 class CityRegion(Base):
@@ -7,3 +10,6 @@ class CityRegion(Base):
     id = Column(Integer, primary_key=True, index=True)
     city_id = Column(Integer, ForeignKey("cities.id", ondelete="CASCADE"), nullable=False)
     region_id = Column(Integer, ForeignKey("regions.id", ondelete="CASCADE"), nullable=False)
+
+    city = relationship("City", backref="city_regions")
+    region = relationship("Region", backref="city_regions")
