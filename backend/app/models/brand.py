da55cdfffe4d6d2a32d352026a988e6dc3ec040f
DiscountHub <k.proj.2019@gmail.com>
2025-07-25 13:17:41 +0400

    Create brand.py

---
+++ backend/app/models/brand.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.db import Base
+
+class Brand(Base):
+    __tablename__ = "brands"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True, nullable=False)
+
+    # связи на будущее
+    # discounts = relationship("Discount", back_populates="brand")
