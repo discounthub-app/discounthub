0c575118ebb829dc17c989038cf022b62772c7cc
DiscountHub <k.proj.2019@gmail.com>
2025-07-25 13:54:39 +0400

    Update favorite.py

--- backend/app/models/favorite.py
+++ backend/app/models/favorite.py
@@ -1,13 +1,14 @@
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship
 from app.db import Base
 
 class Favorite(Base):
     __tablename__ = "favorites"
+    __table_args__ = (UniqueConstraint('user_id', 'discount_id', name='_user_discount_uc'),)
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    product_id = Column(Integer, ForeignKey("products.id"), nullable=False)
+    discount_id = Column(Integer, ForeignKey("discounts.id"), nullable=False)
 
     user = relationship("User", back_populates="favorites")
-    product = relationship("Product", back_populates="favorites")
+    discount = relationship("Discount", back_populates="favorites")
