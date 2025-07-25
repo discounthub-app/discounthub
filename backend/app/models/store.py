03c9bb0ea09cc1b6de91ff6f90f5a5e54a22df6a
DiscountHub <k.proj.2019@gmail.com>
2025-07-25 13:27:12 +0400

    Update store.py

--- backend/app/models/store.py
+++ backend/app/models/store.py
@@ -2,11 +2,11 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from app.db import Base
 
-
 class Store(Base):
     __tablename__ = "stores"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, nullable=False)
+    name = Column(String, unique=True, index=True, nullable=False)
+    url = Column(String, nullable=True)
 
-    # В будущем можно добавить связь: discounts = relationship("Discount", back_populates="store")
+    products = relationship("Product", back_populates="store")
