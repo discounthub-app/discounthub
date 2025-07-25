59621b4b1187b4f55fea630391c72793b578d72b
DiscountHub <k.proj.2019@gmail.com>
2025-07-25 17:25:37 +0400

    Create category.py

---
+++ backend/app/crud/category.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+from app.models.category import Category
+from app.schemas.category import CategoryCreate
+
+
+def get_categories(db: Session):
+    return db.query(Category).all()
+
+
+def create_category(db: Session, category: CategoryCreate):
+    db_category = Category(
+        name=category.name,
+        description=category.description
+    )
+    db.add(db_category)
+    db.commit()
+    db.refresh(db_category)
+    return db_category
