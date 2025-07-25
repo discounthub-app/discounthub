ef75557d7f68fb761028a198b831d5b6e828e2e3
DiscountHub <k.proj.2019@gmail.com>
2025-07-26 01:33:09 +0400

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,10 +1,9 @@
 from pydantic_settings import BaseSettings
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 class Settings(BaseSettings):
-    database_url: str = os.getenv("DATABASE_URL")
+    database_url: str
+
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
