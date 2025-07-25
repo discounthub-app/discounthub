1c0c52f14df22da4fbbd06acdcd1d741b08a7031
DiscountHub <k.proj.2019@gmail.com>
2025-07-25 16:53:31 +0400

    Update discount.py

--- backend/app/crud/discount.py
+++ backend/app/crud/discount.py
@@ -10,13 +10,15 @@ def get_discount(db: Session, discount_id: int):
 def get_discounts(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Discount).offset(skip).limit(limit).all()
 
+
 def create_discount(db: Session, discount: schemas.DiscountCreate):
     db_discount = models.Discount(
         title=discount.title,
         description=discount.description,
         price=discount.price,
         old_price=discount.old_price,
-        seller_id=discount.seller_id
+        seller_id=discount.seller_id,
+        product_id=discount.product_id  # ✅ добавлено
     )
     db.add(db_discount)
     db.commit()
