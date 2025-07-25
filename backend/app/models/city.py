8f64bae54803c7e9a0978aae606a291f61fcc7f9
DiscountHub <k.proj.2019@gmail.com>
2025-07-25 13:36:57 +0400

    Update city.py

--- backend/app/models/city.py
+++ backend/app/models/city.py
@@ -1,1 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.db import Base
 
+class City(Base):
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+
+    # связи на будущее
+    # stores = relationship("Store", back_populates="city")
+    # users = relationship("UserProfile", back_populates="city")
