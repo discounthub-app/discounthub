7a8039b2b490294d8f2ae87fed798c78a18d2bc7
DiscountHub <k.proj.2019@gmail.com>
2025-07-26 16:43:35 +0400

    Create auth.py

---
+++ backend/app/schemas/auth.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel, EmailStr, Field
+
+class UserRegister(BaseModel):
+    email: EmailStr
+    username: str = Field(..., min_length=3, max_length=50)
+    password: str = Field(..., min_length=6)
