e70bb64dd5990545de501103a06242dc33d3227b
DiscountHub <k.proj.2019@gmail.com>
2025-07-27 14:00:07 +0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
-  }
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://app_backend:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
