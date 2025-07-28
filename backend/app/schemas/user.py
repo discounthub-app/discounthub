ef17fadac81891a089f57844a18e297fd813cff6
DiscountHub <k.proj.2019@gmail.com>
2025-07-29 02:14:01 +0400

    Update user.py

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,24 +1,21 @@
 from pydantic import BaseModel, ConfigDict, EmailStr
 from typing import Optional
 
-
 class UserBase(BaseModel):
     username: str
     email: EmailStr
 
-
 class UserCreate(UserBase):
     password: str
-
 
 class UserUpdate(BaseModel):
     username: Optional[str] = None
     email: Optional[EmailStr] = None
     password: Optional[str] = None
 
-
 class UserOut(UserBase):
     id: int
+    is_admin: bool = False  # <--- добавили это поле
 
     model_config = ConfigDict(from_attributes=True)
 
