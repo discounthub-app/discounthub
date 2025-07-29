e2bc83ae5385a76fce3500c5ad6789df6e84e20d
DiscountHub <k.proj.2019@gmail.com>
2025-07-29 17:32:39 +0400

    Update favorite.py

--- backend/app/schemas/favorite.py
+++ backend/app/schemas/favorite.py
@@ -1,11 +1,9 @@
 from pydantic import BaseModel, ConfigDict
 
-# Только discount_id для создания избранного!
 class FavoriteCreate(BaseModel):
     discount_id: int
 
 class FavoriteOut(BaseModel):
     id: int
     discount_id: int
-    # user_id: int — опционально, но фронту обычно не нужен
     model_config = ConfigDict(from_attributes=True)
