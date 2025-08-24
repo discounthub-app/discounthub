80f88fd50bb05df7e7064bf20bea86c48cf7b7a6
DiscountHub <k.proj.2019@gmail.com>
2025-08-24 22:51:20 +0300

    Integrate global error logging in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Подключаем глобальное логирование ошибок
+import { setupGlobalErrorLogging } from "./lib/logger";
+setupGlobalErrorLogging();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
